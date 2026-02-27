ff15c5f90fcdaf4633e1e909d9df545b5ae12249
AadityaRJ01 <1ms23cs002@msrit.edu>
2026-02-27 15:57:44 +0530

    Implemented Brands section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Layout } from "./components/Layout";
 import { Hero } from "./components/sections/Hero";
+import { Brands } from "./components/sections/Brands";
 function App() {
   return (
     <Layout title="EdgeAI">
       <Hero />
-      
+      <Brands/>
     </Layout>
   );
 }
